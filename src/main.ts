111cfa7031f58bbae101eb73e0eca3c838ee2383
Victor Hugo <vh.viveiros@gmail.com>
2022-10-21 20:23:46 -0300

    now it gets the full info

--- src/main.ts
+++ src/main.ts
@@ -11,28 +11,16 @@ const chrome = require("selenium-webdriver/chrome");
     .setChromeOptions(options)
     .build();
   try {
-    for (let c in cookies) {
-      driver.manage().addCookie(cookies[c])
+    for (let c of cookies) {
+      driver.manage().addCookie(c)
     }
     await driver.get("https://www.twitch.tv/drops/campaigns")
     await driver.navigate().refresh()
 
     for (let e of await driver.findElements(
-      webdriver.By.xpath("//h4/preceding::h3[contains(@class, 'tw-title')]"))) {
+      webdriver.By.xpath("//h4/preceding::div[contains(@class, 'Layout-sc-nxg1ff-0 ScAccordionHeaderContents-sc-ja4t0c-0')]"))) {
       console.log(await e.getText())
-      // const next = await driver.findElement(webdriver.By.xpath("./child::*"))
-      // console.log(await next.getText())
     }
-
-    // console.log(await driver.findElement(
-    //   webdriver.By.xpath("//div[contains(@class, 'Layout-sc-nxg1ff-0 iFiRut')]/preceding::p[contains(@class, 'CoreText-sc-cpl358-0')]/preceding::div[contains(@class, 'Layout-sc-nxg1ff-0 iFiRut')]")).getText())
-
-    // for (let e of await driver.findElements(
-    //   webdriver.By.xpath("//div[contains(@class, 'Layout-sc-nxg1ff-0 iFiRut')]/preceding::p[contains(@class, 'CoreText-sc-cpl358-0')]/preceding::div[contains(@class, 'Layout-sc-nxg1ff-0 iFiRut')]"))) {
-    //   console.log(await e.getText());
-    // }
-
-
 
   } finally {
     await driver.quit();
