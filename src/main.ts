7722bd96bd1cad4035f64fe74be5357af7ecb8fe
Victor Hugo <vh.viveiros@gmail.com>
2022-10-25 16:30:57 -0300

    code is now generating a json object

--- src/main.ts
+++ src/main.ts
@@ -16,11 +16,30 @@ const chrome = require("selenium-webdriver/chrome");
     }
     await driver.get("https://www.twitch.tv/drops/campaigns")
     await driver.navigate().refresh()
+    var return_result = []
 
     for (let e of await driver.findElements(
       webdriver.By.xpath("//h4/preceding::div[contains(@class, 'Layout-sc-nxg1ff-0 ScAccordionHeaderContents-sc-ja4t0c-0')]"))) {
-      console.log(await e.getText())
+      const result = await e.getText()
+      var result_split = ["", "", ""]
+      var period_split = ["", ""]
+
+      try {
+        result_split = result.split("\n")
+        period_split = result_split[2].split(" - ")
+      }
+      catch { }
+
+      const result_json = {
+        game: result_split[0],
+        distributor: result_split[1],
+        start: period_split[0],
+        end: period_split[1]
+      }
+
+      return_result.push(result_json)
     }
+    console.log(return_result)
 
   } finally {
     await driver.quit();
