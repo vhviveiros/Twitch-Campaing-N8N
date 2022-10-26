5eda2a35ef57369934ee3122930c9295c55678bf
Victor Hugo <vh.viveiros@gmail.com>
2022-10-26 18:14:09 -0300

    ;-;

--- build/src/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const data_fetch_1 = require("./data_fetch");
-(async function example() {
-    const result = new data_fetch_1.DataFetch().fetch();
-    console.log(await result);
-})();
-//# sourceMappingURL=main.js.map
